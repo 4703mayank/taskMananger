03e47417d852271a3b7f29ba2ecfe04e5cbd0f30
Mayank Sharma <4703mayank@gmail.com>
2025-12-03 00:00:38 +0530

    Basic API's are settled up.

--- src/main/java/org/springboot/taskmanager/contollers/TaskController.java
+++ src/main/java/org/springboot/taskmanager/contollers/TaskController.java
@@ -1,36 +1,50 @@
 package org.springboot.taskmanager.contollers;
 
 import org.springboot.taskmanager.entities.TaskEntity;
-import org.springboot.taskmanager.services.TaskService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
-    private final TaskService taskService;
-    public TaskController (TaskService taskService){
-        this.taskService = taskService;
+
+    private Map<Integer,TaskEntity> tasks = new HashMap<>();
+
+    @PostMapping
+    public boolean createTask(@RequestBody TaskEntity newTask){
+        tasks.put(newTask.getId(), newTask);
+        return true;
     }
+
 
     @GetMapping("/getAll")
     public List<TaskEntity> getAll(){
-        return new ArrayList<>(taskService.getTasks());
+        return new ArrayList<>(tasks.values());
     }
 
-    @GetMapping("")
-    public TaskEntity getById(int id){
-        if(taskService.getTasksById(id) == null)return null ;
-        return taskService.getTasksById(id);
+    @GetMapping("id/{id}")
+    public TaskEntity getById(@PathVariable int id){
+        if(tasks.get(id) == null)return null ;
+        return tasks.get(id);
     }
 
-    @PostMapping("")
-    public void saveTask(String name, String description){
 
+    @DeleteMapping("/id/{id}")
+    public TaskEntity deleteTaskById(@PathVariable int id){
+        if(tasks.get(id) == null)return null;
+        TaskEntity deletedTask = tasks.get(id);
+        tasks.remove(id);
+        return deletedTask;
+    }
+
+    @PutMapping("/id/{id}")
+    public TaskEntity updateTaskById(@PathVariable int id, @RequestBody TaskEntity newTask){
+        tasks.put(id,newTask);
+        return tasks.get(id);
     }
 }
+
